8f351d0e31d71b13a75ceb5059faa6379c916a17
Masahiro Wada <argon.argon.argon@gmail.com>
2022-01-07 00:33:25 +0900

    add kmeans++ support

--- procs.go
+++ procs.go
@@ -8,15 +8,50 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
-func calcInitialCentroids(X *mat.Dense, nClusters uint) *mat.Dense {
+func calcRandomInitialCentroids(X *mat.Dense, nClusters uint) *mat.Dense {
 	_, nFeatures := X.Dims()
 	centroids := mat.NewDense(int(nClusters), nFeatures, nil)
 	for i := 0; i < int(nClusters); i++ {
 		for j := 0; j < int(nFeatures); j++ {
-			centroids.Set(i, j, rand.NormFloat64())
+			centroids.Set(i, j, math.Abs(rand.NormFloat64()))
 		}
 	}
 	return centroids
+}
+
+func calcKmeansPlusPlusInitialCentroids(X *mat.Dense, nClusters uint) *mat.Dense {
+	nSamples, featDim := X.Dims()
+	centroids := mat.NewDense(int(nClusters), featDim, nil)
+	centroids.SetRow(0, X.RawRowView(rand.Intn(int(nSamples))))
+	for i := 1; i < int(nClusters); i++ {
+		accDistances := make([]float64, nSamples)
+		for j := 0; j < int(nSamples); j++ {
+			minDinstance := math.MaxFloat64
+			for k := 0; k < i; k++ {
+				distance := calcL2Distance(X.RowView(j), centroids.RowView(k))
+				minDinstance = math.Min(minDinstance, distance)
+			}
+			if j == 0 {
+				accDistances[j] = minDinstance
+			} else {
+				accDistances[j] = accDistances[j-1] + minDinstance
+			}
+		}
+
+		r := accDistances[nSamples-1] * rand.Float64()
+		for j := 0; j < int(nSamples); j++ {
+			if r <= accDistances[j] {
+				centroids.SetRow(i, X.RawRowView(j))
+				break
+			}
+		}
+	}
+
+	return centroids
+}
+
+func calcInitialCentroids(X *mat.Dense, nClusters uint) *mat.Dense {
+	return calcKmeansPlusPlusInitialCentroids(X, nClusters)
 }
 
 func assignCluster(X *mat.Dense, centroids *mat.Dense, classes []uint, indices []uint, calcDistance func(X, Y mat.Vector) float64) {
@@ -35,7 +70,7 @@ func assignCluster(X *mat.Dense, centroids *mat.Dense, classes []uint, indices [
 	}
 }
 
-func accumulateSamples(X *mat.Dense, centroids *mat.Dense, nSamplesInCluster []uint, classes []uint, indices []uint) {
+func accumulateSamplesInCluster(X *mat.Dense, centroids *mat.Dense, nSamplesInCluster []uint, classes []uint, indices []uint) {
 	for _, i := range indices {
 		feat := X.RowView(int(i))
 		cluster := int(classes[i])
