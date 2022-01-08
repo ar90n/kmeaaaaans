47c0f6d67b7b2d9185020c80c04ae3fb15379a55
Masahiro Wada <argon.argon.argon@gmail.com>
2022-01-08 13:03:27 +0900

    add minibatch support

--- benchmark/scikit-learn/main.py
+++ benchmark/scikit-learn/main.py
@@ -1,29 +1,62 @@
+from __future__ import annotations
+
 import csv
 import json
 from pathlib import Path
 from time import time
+from contextlib import contextmanager
+from enum import Enum
+from typing import Optional
 
 import numpy as np
 import typer
-from numpy.lib.shape_base import row_stack
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, MiniBatchKMeans
 
 
-def main(n_clusters: int, input_path: Path, output_path: Path):
-    with input_path.open("r") as fp:
-        data = np.array([list(map(float, row)) for row in csv.reader(fp)])
-
-    kmeans = KMeans(n_clusters=n_clusters)
-    kmeans.fit(data)
-    n = 1
+def benchmark(kmeans: KMeans, data: np.array, n: int):
     t0 = time()
     for _ in range(n):
         kmeans.fit(data)
     duration = int(1e9 * (time() - t0) // n)
-    print(json.dumps({"duration_ns": duration}))
+    return json.dumps({"duration_ns": duration})
 
-    with output_path.open("w") as fp:
-        csv.writer(fp).writerows(kmeans.cluster_centers_)
+@contextmanager
+def csv_io_context(input_path: Path, output_path: Path):
+    def _task():
+        cluster_centers, benchmark_result = yield
+        with output_path.open("w") as fp:
+            csv.writer(fp).writerows(cluster_centers)
+        print(benchmark_result)
+
+    with input_path.open("r") as fp:
+        data = np.array([list(map(float, row)) for row in csv.reader(fp)])
+
+    generator = _task()
+    next(generator)
+    try:
+        yield data, generator
+    except StopIteration:
+        pass
+
+class Algorithm(str, Enum):
+    vanila = "vanila"
+    minibatch = "minibatch"
+
+
+def main(n_clusters: int, input_path: Path, output_path: Path, algorithm: Algorithm = Algorithm.vanila, batch_size: Optional[int] = None):
+    KmeansClass = {
+        Algorithm.vanila: KMeans,
+        Algorithm.minibatch: MiniBatchKMeans,
+    }[algorithm]
+
+    args = {}
+    if batch_size is not None:
+        args["batch_size"] = batch_size
+    with csv_io_context(input_path, output_path) as (data, c):
+        kmeans = KmeansClass(n_clusters=n_clusters, **args)
+        kmeans.fit(data)
+        benchmark_result = benchmark(kmeans, data, 8)
+        c.send((kmeans.cluster_centers_, benchmark_result))
 
 
 if __name__ == "__main__":
