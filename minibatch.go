c2fec54790c24e810720d45c5a8cb8921bb1826e
Masahiro Wada <argon.argon.argon@gmail.com>
2022-01-10 13:33:04 +0900

    add minibatch

--- minibatch.go
+++ minibatch.go
@@ -2,8 +2,9 @@ package kmeaaaaans
 
 import (
 	"math/rand"
-	"sync"
+	"runtime"
 
+	"github.com/panjf2000/ants/v2"
 	"gonum.org/v1/gonum/mat"
 )
 
@@ -26,28 +27,172 @@ func makeBatchIndice(nSamples, batchSize uint) []uint {
 }
 
 func (k *miniBatchKmeans) Fit(X *mat.Dense) TrainedKmeans {
+	nSamples, featDim := X.Dims()
 	nextCentroids := calcInitialCentroids(X, k.nClusters, k.initAlgorithm)
-	centroids := mat.NewDense(nextCentroids.RawMatrix().Rows, nextCentroids.RawMatrix().Cols, nil)
+	centroids := mat.NewDense(int(k.nClusters), featDim, nil)
 
-	indices := makeBatchIndice(uint(X.RawMatrix().Rows), k.batchSize)
-	chunks := makeChunks(indices, 1024)
 	classes := make([]uint, X.RawMatrix().Rows)
+
+	type AssignClusterHandlerParams struct {
+		X         *mat.Dense
+		Centroids *mat.Dense
+		Classes   []uint
+		Indices   []uint
+	}
+
+	defer ants.Release()
+	pool, _ := ants.NewPool(runtime.NumCPU())
+
+	//accSamples := mat.NewDense(int(k.nClusters), featDim, nil)
+	accNSamplesInCluster := make([]uint, k.nClusters)
+	nSamplesInCluster := make([]uint, k.nClusters)
+	indices := makeBatchIndice(uint(X.RawMatrix().Rows), k.batchSize)
+	chunks := makeChunks(indices, 256)
+	minInertia := float64(1e10)
+	minRuns := 0
+	//fmt.Println("------------")
 	for i := 0; i < int(k.maxIterations) && k.tolerance < calcError(centroids, nextCentroids); i++ {
+		for j := 0; j < len(indices); j++ {
+			indices[j] = uint(rand.Intn(int(nSamples)))
+		}
+		for j := 0; j < len(chunks); j++ {
+			for k := 0; k < len(chunks[j]); k++ {
+				chunks[j][k] = indices[j*len(chunks[j])+k]
+			}
+		}
+
 		centroids, nextCentroids = nextCentroids, centroids
 		nextCentroids.Zero()
 
-		nSamplesInCluster := make([]uint, k.nClusters)
-		var wg sync.WaitGroup
+		channel := make([]chan float64, 0)
 		for _, chunk := range chunks {
-			wg.Add(1)
-			go func(chunk []uint) {
-				defer wg.Done()
-				assignCluster(X, centroids, classes, chunk, calcL2Distance)
-				accumulateSamplesInCluster(X, nextCentroids, nSamplesInCluster, classes, chunk)
-			}(chunk)
+			//wg.Add(1)
+			chunk := chunk
+			channel = append(channel, make(chan float64, 1))
+			cur := channel[len(channel)-1]
+			//params := AssignClusterHandlerParams{
+			//	X:         X,
+			//	Centroids: centroids,
+			//	Classes:   classes,
+			//	Indices:   chunk,
+			//}
+			//assignClusterHandler.Invoke(params)
+			pool.Submit(func() {
+				ret := assignCluster(X, centroids, classes, chunk, calcL2Distance)
+				//fmt.Println(ret)
+				cur <- ret
+				close(cur)
+			})
 		}
-		wg.Wait()
-		updateCentroid(X, nextCentroids, nSamplesInCluster)
+
+		inertia := 0.0
+		for _, c := range channel {
+			for v := range c {
+				inertia += v
+				//fmt.Println(inertia)
+			}
+		}
+		if inertia < minInertia {
+			minInertia = inertia
+			minRuns = 0
+		} else {
+			minRuns++
+		}
+		//fmt.Println(i, inertia, minInertia, minRuns)
+		if minRuns > 10 {
+			break
+		}
+		//	var wg sync.WaitGroup
+		//	assignClusterHandler, _ := ants.NewPoolWithFunc(runtime.NumCPU(), func(i interface{}) {
+		//		defer wg.Done()
+		//		param := i.(AssignClusterHandlerParams)
+		//		assignCluster(param.X, param.Centroids, param.Classes, param.Indices, calcL2Distance)
+		//	})
+		//	defer assignClusterHandler.Release()
+		//
+		//		}
+		//		wg.Wait()
+
+		for i := 0; i < len(nSamplesInCluster); i++ {
+			nSamplesInCluster[i] = 0
+		}
+
+		for _, i := range indices {
+			cluster := int(classes[i])
+			nSamplesInCluster[cluster]++
+			accNSamplesInCluster[cluster]++
+			centroidData := nextCentroids.RawRowView(cluster)
+			featData := X.RawRowView(int(i))
+			for j := 0; j < len(featData); j++ {
+				centroidData[j] += featData[j]
+			}
+		}
+		//matPrint(nextCentroids)
+		for k := 0; k < len(nSamplesInCluster); k++ {
+			if 0 < nSamplesInCluster[k] {
+				scale := float64(nSamplesInCluster[k]) / float64(accNSamplesInCluster[k])
+				scale2 := 1.0 / float64(accNSamplesInCluster[k])
+				//if i == 0 {
+				//	scale = 1.0 / float64(accNSamplesInCluster[k])
+				//}
+				centroidData := nextCentroids.RawRowView(k)
+				oldCentroidData := centroids.RawRowView(k)
+				//fmt.Println(k, centroidData)
+				//fmt.Println(k, nSamplesInCluster[k], scale, scale2)
+				for j := 0; j < nextCentroids.RawMatrix().Cols; j++ {
+					centroidData[j] = scale2*centroidData[j] + (1-scale)*oldCentroidData[j]
+				}
+			} else {
+				nextCentroids.SetRow(k, centroids.RawRowView(k))
+			}
+		}
+		//fmt.Println(nSamplesInCluster)
+		//fmt.Println(accNSamplesInCluster)
+		//matPrint(centroids)
+		//matPrint(nextCentroids)
+
+		//		for _, ii := range indices {
+		//			cluster := int(classes[ii])
+		//			//nSamplesInCluster[cluster]++
+		//			//accNSamplesInCluster[cluster]++
+		//			accNSamplesInCluster[cluster] += uint(1 + i)
+		//			//centroidData := nextCentroids.RawRowView(cluster)
+		//			centroidData := accSamples.RawRowView(cluster)
+		//			featData := X.RawRowView(int(ii))
+		//			for j := 0; j < len(featData); j++ {
+		//				//centroidData[j] += featData[j]
+		//				centroidData[j] += float64(1+i) * featData[j]
+		//			}
+		//		}
+		//		//fmt.Println(accNSamplesInCluster)
+		//
+		//		//for ii := 0; ii < len(nSamplesInCluster); ii++ {
+		//		for ii := 0; ii < len(accNSamplesInCluster); ii++ {
+		//			//if 0 < nSamplesInCluster[ii] {
+		//			if 0 < accNSamplesInCluster[ii] {
+		//				scale := 1.0 / float64(accNSamplesInCluster[ii])
+		//				centroidData := nextCentroids.RawRowView(ii)
+		//				accData := accSamples.RawRowView(ii)
+		//				for j := 0; j < featDim; j++ {
+		//					centroidData[j] = scale * accData[j]
+		//				}
+		//				//for j := 0; j < nextCentroids.RawMatrix().Cols; j++ {
+		//				//	nextCentroids.Set(ii, j, nextCentroids.At(ii, j)+centroids.At(ii, j)*float64(accNSamplesInCluster[ii]))
+		//				//}
+		//				//accNSamplesInCluster[ii] += nSamplesInCluster[ii]
+		//				//scale := 1.0 / float64(accNSamplesInCluster[ii])
+		//				//for j := 0; j < nextCentroids.RawMatrix().Cols; j++ {
+		//				//	nextCentroids.Set(ii, j, nextCentroids.At(ii, j)*scale)
+		//				//}
+		//			} else {
+		//				nextCentroids.SetRow(ii, centroids.RawRowView(ii))
+		//			}
+		//		}
+		//matPrint(nextCentroids)
+		//if 2 < i {
+		//	break
+		//}
+		//fmt.Println(i, calcError(centroids, nextCentroids))
 	}
 	centroids = nextCentroids
 
--- procs.go
+++ procs.go
@@ -77,20 +77,36 @@ func calcInitialCentroids(X *mat.Dense, nClusters uint, initAlgorithm InitAlgori
 	}
 }
 
-func assignCluster(X *mat.Dense, centroids *mat.Dense, classes []uint, indices []uint, calcDistance func(X, Y []float64) float64) {
-	nClusters, _ := centroids.Dims()
+func assignCluster(X *mat.Dense, centroids *mat.Dense, classes []uint, indices []uint, calcDistance func(X, Y []float64) float64) float64 {
+	nClusters, featDim := centroids.Dims()
+	inertia := 0.0
 	for _, i := range indices {
 		minDist := math.MaxFloat64
 		minClass := uint(0)
+		bc := make([]float64, featDim)
 		for j := 0; j < int(nClusters); j++ {
 			dist := calcDistance(X.RawRowView(int(i)), centroids.RawRowView(j))
 			if dist < minDist {
 				minDist = dist
 				minClass = uint(j)
+				for q := 0; q < featDim; q++ {
+					bc[q] = centroids.At(j, q)
+				}
 			}
 		}
+		inertia += minDist
+		//fmt.Printf("   %f %f ", inertia, minDist)
+		//fmt.Print(bc)
+		//fmt.Print(" ")
+		//fmt.Println(X.RawRowView(int(i)))
 		classes[i] = minClass
+		//if q < 3 {
+		//	fmt.Println(i, inertia, minDist)
+		//}
 	}
+	//fmt.Println("--------------------")
+
+	return inertia
 }
 
 func accumulateSamplesInCluster(X *mat.Dense, centroids *mat.Dense, nSamplesInCluster []uint, classes []uint, indices []uint) {
@@ -124,17 +140,29 @@ func updateCentroid(X *mat.Dense, centroids *mat.Dense, nSamplesInCluster []uint
 //}
 func calcL2Distance(X, Y []float64) float64 {
 	acc := 0.0
-	for i := 0; i < len(X); i++ {
+	i := 0
+	for ; i < len(X)%4; i++ {
+		//	for ; i < len(X); i++ {
 		diff := X[i] - Y[i]
 		acc += diff * diff
 	}
+
+	for ; i < len(X); i += 4 {
+		diff0 := X[i] - Y[i]
+		diff1 := X[i+1] - Y[i+1]
+		diff2 := X[i+2] - Y[i+2]
+		diff3 := X[i+3] - Y[i+3]
+		acc += diff0*diff0 + diff1*diff1 + diff2*diff2 + diff3*diff3
+	}
+
 	return math.Sqrt(acc)
 }
 
 func calcError(X, Y *mat.Dense) float64 {
-	diff := mat.NewDense(X.RawMatrix().Rows, X.RawMatrix().Cols, nil)
-	diff.Sub(X, Y)
-	return mat.Norm(diff, 2)
+	//	diff := mat.NewDense(X.RawMatrix().Rows, X.RawMatrix().Cols, nil)
+	//	diff.Sub(X, Y)
+	//	return mat.Norm(diff, 2) / mat.Norm(X, 2)
+	return calcL2Distance(X.RawMatrix().Data, Y.RawMatrix().Data) / mat.Norm(X, 2)
 }
 
 func matPrint(X mat.Matrix) {
